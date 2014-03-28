ba87247fda97a3007a0e447484902ffff046d0b2
Andrzej Sulecki <asulecki@gmail.com>
2014-03-29 00:01:07 +0100

    first part working

--- mierniczyc.c
+++ mierniczyc.c
@@ -1,5 +1,184 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
+
+void failwith(const char * msg){
+	fprintf(stderr, "FAILED WITH: ");
+	fprintf(stderr, "%s\n", msg);
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, const char *argv[])
 {
+	char buffer[100];
+	//Obtain UDP port TCP port server addres
+	//TODO
+
+	short udp_port;
+	short tcp_port;
+	char tcp_port_char[10];
 	
+
+	char host[80];
+
+	printf("UDP Port: ");
+	scanf("%d", &udp_port);
+
+	printf("TCP Port: ");
+	scanf("%s", &tcp_port_char);
+	tcp_port = atoi(tcp_port_char);
+	
+
+	printf("Server: ");
+	scanf("%s", &host);
+
+	//Open UDP Socket
+
+	int udp_socket;
+
+	struct sockaddr_in udp_sockaddr;
+
+	socklen_t udp_sockaddr_len;
+
+
+	udp_sockaddr.sin_family = AF_INET;
+	udp_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	udp_sockaddr.sin_port = htons(udp_port);
+	
+	udp_sockaddr_len = (socklen_t) sizeof(udp_sockaddr);
+
+	udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	if (udp_socket < 0)
+		failwith("socket");
+
+	if (bind(
+		udp_socket, 
+		(struct sockaddr *)&udp_sockaddr, 
+		udp_sockaddr_len
+		) < 0) 
+	{
+		failwith("BIND");
+	}
+
+	//Opening UDP done
+
+	//Connect to server by TCP
+
+	struct addrinfo addr_tcp_hints;
+	struct addrinfo *addr_tcp;
+
+	int tcp_socket;
+
+	memset(&addr_tcp_hints, 0, sizeof(struct addrinfo));
+
+	addr_tcp_hints.ai_family	= AF_INET;
+	addr_tcp_hints.ai_socktype	= SOCK_STREAM;
+	addr_tcp_hints.ai_protocol	= IPPROTO_TCP;
+
+	if (getaddrinfo(host, tcp_port_char, &addr_tcp_hints, &addr_tcp) != 0)
+		failwith("getaddrinfo");
+
+	tcp_socket = socket(addr_tcp->ai_family, addr_tcp->ai_socktype, addr_tcp->ai_protocol);
+
+	if (tcp_socket < 0)
+		failwith("socket");
+
+	if (connect(tcp_socket, addr_tcp->ai_addr, addr_tcp->ai_addrlen) < 0)
+		failwith("connect");
+
+	freeaddrinfo(addr_tcp);
+	
+	printf("Sending %d %d\n", udp_port, htons(udp_port));
+	sprintf(buffer, "%d", htons(udp_port));
+	if (write(tcp_socket, buffer, strlen(buffer)) < 0)
+		failwith("write");
+
+	//Connected
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	const char * host	= argv[3];
+
+	int udp_socket;
+
+	struct addrinfo addr_hints;
+	struct addrinfo * addr;
+	
+	struct sockaddr_in client_udp_address;
+	struct sockaddr_in server_address;
+
+	socklen_t client_udp_address_len;
+
+	(void) memset(&addr_hints, 0, sizeof(struct addrinfo));
+
+	addr_hints.ai_family	= AF_INET;
+	addr_hints.ai_socktype	= SOCK_DGRAM;
+	addr_hints.ai_protocol	= IPPROTO_UDP;
+	addr_hints.ai_flags	= 0;
+	addr_hints.ai_addrlen	= 0;
+	addr_hints.ai_addr	= NULL;
+	addr_hints.ai_canonname	= NULL;
+	addr_hints.ai_next	= NULL;
+	
+	if (getaddrinfo(host, NULL, &addr_hints, &addr) != 0) {
+		failwith("getaddrinfo");
+	}
+
+	client_udp_address.sin_family = AF_INET;
+	client_udp_address.sin_addr.s_addr =
+		((struct sockaddr_in*) (addr->ai_addr))->sin_addr.s_addr;
+	client_udp_address.sin_port = htons(udp_port);
+	
+	client_udp_address_len = (socklen_t) sizeof(client_udp_address);
+
+	//Open UDP socket
+	//TODO
+
+	udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	if (bind(udp_socket, client_udp_address, client_udp_address_len) != 0) {
+		failwith("BIND");
+	}
+	//Connect to server
+	//TODO
+
+	//Senddata from stdin
+	//TODO
+
+	//Receive UDP from serwer
+	//TODO
+
+	//Write on stdout received number
+	//TODO
+	freeaddrinfo(addr);*/
 	return 0;
 }
--- mierniczys.c
+++ mierniczys.c
@@ -1,5 +1,97 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+
+#define BUFFER_SIZE	1000
+#define QUEUE_LENGTH	5
+void failwith(const char * msg){
+	fprintf(stderr, "FAILED WITH: ");
+	fprintf(stderr, "%s\n", msg);
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, const char *argv[])
 {
+
+
+	short tcp_port;
+	printf("Type in port number: \n");
+	scanf("%d", &tcp_port);
+
+	int tcp_socket;
+
+	struct sockaddr_in client_udp_address;
+	struct sockaddr_in tcp_addr;
+
+	socklen_t tcp_addr_len;
+
+
+	tcp_addr.sin_family = AF_INET;
+	tcp_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	tcp_addr.sin_port = htons(tcp_port);
+
+	tcp_addr_len = (socklen_t) sizeof(tcp_addr);
+
+	tcp_socket = socket(PF_INET, SOCK_STREAM, 0);
 	
+	if (tcp_socket < 0)
+		failwith("socket");
+
+	if (bind(
+		tcp_socket, 
+		(struct sockaddr *)&tcp_addr, 
+		tcp_addr_len
+		) < 0) 
+	{
+		failwith("BIND");
+	}
+
+	//Listen for connection TCP
+
+	if (listen(tcp_socket, QUEUE_LENGTH) < 0)
+		failwith("listen");
+
+	//Get port number UDP
+	//TODO
+
+	int client_sock;
+	struct sockaddr_in client_address;
+	socklen_t client_address_len = sizeof(client_address);
+
+	client_sock = accept(
+			tcp_socket, 
+			(struct sockaddr *) &client_address, 
+			&client_address_len);
+
+	if (client_sock < 0)
+		failwith("accept");
+	short udp_port;
+
+	int len;
+	char buffer[BUFFER_SIZE];
+
+	len = read(client_sock, buffer, BUFFER_SIZE);
+	if (len < 0)
+		failwith("read");
+	printf("read udp port: %s\n", buffer);
+	udp_port = ntohs(atoi(buffer));
+	
+	printf("read udp port: %d\n", udp_port);
+	//Get data
+	//TODO
+
+	//Calculate data size
+	//TODO
+
+	//Connect to client on UDP
+	//TODO
+
+	//Send data size
+	//TODO
+
+	//Close all and finish
 	return 0;
 }
